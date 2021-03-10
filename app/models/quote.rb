ec52630f372821651b9d36f9333035e8e5f0a17f
DaveVaval <grimmjowx9@hotmail.com>
2021-03-10 16:55:02 -0500

    Fixed sendgrid issue

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,27 +1,2 @@
 class Quote < ApplicationRecord
-  require 'zendesk_api'
-  # after_create :create_quote_ticket
-
-  def create_quote_ticket
-      client = ZendeskAPI::Client.new do |config|
-          config.url = ENV['ZENDESK_URL']
-          config.username = ENV['ZENDESK_USERNAME']
-          config.token = ENV['ZENDESK_TOKEN']
-      end
-      ZendeskAPI::Ticket.create!(client, 
-          :subject => "#{self.company_name}", 
-          :comment => { 
-              :value => "#{self.company_name} can be reached at email #{self.email}. 
-                  The chosen building type is #{self.building_type} with product line #{self.product_line}. 
-                  Number of suggested elevator is #{self.elevator_amount} and total price is #{self.final_price}. \n
-                  For More Information, refers to Quote ##{self.id}."
-          }, 
-          :requester => { 
-              "name": self.company_name, 
-              "email": self.email         
-            },
-          :priority => "normal",
-          :type => "task"
-          )
-  end
 end
