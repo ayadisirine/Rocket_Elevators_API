ec1a8546279a8a005da9d8a40baca1bd5211326c
InesIzere <izere.ines@gmail.com>
2021-03-12 23:55:31 -0500

    bonus

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins '*'
 
-    resource '*',
-    headers: :any,
-    method: [:get, :post, :put, :patch, :option, :delete, :head, :put] 
-  end
-end
+#     resource '*',
+#     headers: :any,
+#     method: [:get, :post, :put, :patch, :option, :delete, :head, :put] 
+#   end
+# end
