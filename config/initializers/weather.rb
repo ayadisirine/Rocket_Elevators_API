2c94bc43f92d8a6b26c5a1b9e926e53de24ae7b3
YoannTessuer <yoann.tessier@hotmail.com>
2021-03-12 19:53:53 -0500

    Minor fix

--- config/initializers/weather.rb
+++ config/initializers/weather.rb
@@ -2,5 +2,5 @@ require "openweather2"
 
 Openweather2.configure do |config|
   config.endpoint = 'http://api.openweathermap.org/data/2.5/weather'
-  config.apikey = YOUR APP ID
+  config.apikey = ENV["OPEN_WEATHER_APIKEY"]
 end
